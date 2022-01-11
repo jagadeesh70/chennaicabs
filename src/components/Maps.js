b54586504aa270d666ca836ad9182e36b60db0a9
jagadeesh70 <54113315+jagadeesh70@users.noreply.github.com>
2022-01-12 00:50:17 +0530

    footer adder

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import { GoogleMap, useJsApiLoader, DirectionsService,DirectionsRenderer } from '@react-google-maps/api'
+import React, { useState, useCallback } from 'react'
 import "./Maps.css"
 
-function Maps() {
+const isLoaded = true
+const containerStyle = {
+    width: '300px',
+    height: '300px'
+}
+
+
+const Maps = () => {
+    const DirectionsService = new window.google.maps.DirectionsService()
+    let [direction,setDirection] = useState("")
+    DirectionsService.route(
+            {
+                origin: { lat: 12.5244, lng:  77.3792 },
+                destination: { lat: 18.5244, lng:  80.3792 },
+                travelMode: window.google.maps.TravelMode.DRIVING
+            },
+            (result,status) =>{
+                if(status === window.google.maps.DirectionsStatus.OK){
+                    setDirection(result)
+                    console.log(result)
+                }else{
+                    console.log(`error ::: ${result}`)
+                }
+            }
+        )
+
     return (
-        <div>
-            hello
+        <div className='map-container'>
+            <div className="googlemap">
+                <GoogleMap 
+                defaultZoom={10}
+                mapContainerStyle={containerStyle}
+                
+                >
+                    {direction && <DirectionsRenderer directions={direction}/>}
+                </GoogleMap>
+            </div>
         </div>
     )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Maps from '../components/Maps'
+
 
 function Homepage() {
     return (
         <div>
             <Header></Header>
             <Maps></Maps>
+            <Footer></Footer>
         </div>
     )
 }
