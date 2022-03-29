b8ffb8e8b6548a17024df6cd8dd42fc5234ae058
B1nLad3n <54113315+jagadeesh70@users.noreply.github.com>
2022-03-29 19:54:37 +0530

    Update firebase-config.js

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAcu0mD1zlf_Zw_NMyJuMTnIuW22mlSW0w",
-  authDomain: "chennai-cabs-new.firebaseapp.com",
-  projectId: "chennai-cabs-new",
-  storageBucket: "chennai-cabs-new.appspot.com",
-  messagingSenderId: "1099372026924",
-  appId: "1:1099372026924:web:339edfabc29672d0ea4589",
-  measurementId: "G-VHW65J5Q7R",
+  YOUR_KEY
 };
 
 const app = initializeApp(firebaseConfig);
